a18c0ee0b67eb843836fd05e7b18b945116974ab
kouroshelkaie <kourosh.elkaie@gmail.com>
2022-02-01 12:25:21 +0330

    flickity slider

---
+++ scripts/flickity.js
@@ -0,0 +1,14 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: 4000,
+  accessibility: true,
+  resize: true,
+  setGallerySize: true,
+});
+
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
