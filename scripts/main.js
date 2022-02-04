97aa763179d2f3f792cd406921a9c5acf797156d
kouroshelkaie <kourosh.elkaie@gmail.com>
2022-02-04 16:52:24 +0330

    brand/map section

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,11 @@ var typed = new Typed('#typed', {
   loop:true,
   shuffle:true,
   typeSpeed:200,
+  backSpeed: 150,
+});
+
+
+/* particles.js*/
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
 });
