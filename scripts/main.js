d895f8971327ba7c5ca4bf63e9dfc7b6b42089a4
kouroshelkaie <kourosh.elkaie@gmail.com>
2022-02-07 16:15:43 +0330

    portfolio filter search

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,37 @@ window.addEventListener("scroll",()=>{
     btn.style.display = "none"
   }
 })
+
+// portfolio filter search
+let form = document.querySelector("#form")
+const input = document.querySelector("#search")
+let items = document.querySelectorAll("#searchItem")
+let container = []
+
+// push items too empty array
+items.forEach((item)=>{
+  container.push(item.textContent)
+})
+// dom function declaration
+let dom = (filteredItems)=>{
+  items.forEach((item)=>{
+    item.style.backgroundColor = "#fff"
+    item.style.color = "#333"
+    filteredItems.forEach((ths)=>{
+      if(item.textContent === ths) {
+        item.style.backgroundColor = "#eb3b55"
+        item.style.color = "#fff"
+      } 
+    })
+  })
+}
+
+form.addEventListener('submit',(e)=>{
+e.preventDefault()
+  let inputValue = input.value
+  let filteredItems = container.filter((item)=>{
+    return item.includes(inputValue)
+  })
+  dom(filteredItems)
+})
+
